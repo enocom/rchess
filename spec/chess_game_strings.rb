9aae31d9b7af5641a323689b4dbed3a94e9386fe
Eno Compton 4 <eno4@ecom.com>
2013-12-27 15:08:21 -0700

    Add chess board with nc3

--- spec/chess_game_strings.rb
+++ spec/chess_game_strings.rb
@@ -26,6 +26,29 @@ module ChessGameStrings
     board
   end
 
+  def knight_to_c3
+    <<-board
+   + - + - + - + - + - + - + - + - +
+8  | R | N | B | K | Q | B | N | R |
+   + - + - + - + - + - + - + - + - +
+7  | P | P | P | P | P | P | P | P |
+   + - + - + - + - + - + - + - + - +
+6  |   |   |   |   |   |   |   |   |
+   + - + - + - + - + - + - + - + - +
+5  |   |   |   |   |   |   |   |   |
+   + - + - + - + - + - + - + - + - +
+4  |   |   |   |   |   |   |   |   |
+   + - + - + - + - + - + - + - + - +
+3  |   |   | n |   |   |   |   |   |
+   + - + - + - + - + - + - + - + - +
+2  | p | p | p | p | p | p | p | p |
+   + - + - + - + - + - + - + - + - +
+1  | r |   | b | k | q | b | n | r |
+   + - + - + - + - + - + - + - + - +
+     a   b   c   d   e   f   g   h
+    board
+  end
+
   def user_prompt
     "\nEnter a move: "
   end
