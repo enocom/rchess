8e5c4cc95c0aa88141458182339af4ffb0ff888d
Eno Compton 4 <eno4@ecom.com>
2013-12-31 23:12:29 -0800

    Add rough code to cover knight's movement

--- lib/rchess/knight.rb
+++ lib/rchess/knight.rb
@@ -14,5 +14,75 @@ module Rchess
     def letter
       @lettercase == :uppercase ? "N" : "n"
     end
+
+    def can_move_to_position?(start_pos, end_pos)
+      up_up_left?(start_pos, end_pos) ||
+        up_up_right?(start_pos, end_pos) ||
+        up_left_left?(start_pos, end_pos) ||
+        up_right_right?(start_pos, end_pos) ||
+        down_left_left?(start_pos, end_pos) ||
+        down_right_right?(start_pos, end_pos) ||
+        down_down_left?(start_pos, end_pos) ||
+        down_down_right?(start_pos, end_pos)
+    end
+
+    private
+    def rotate_letter(letter, direction, offset)
+      letters = %w(a b c d e f g h)
+      index_of_current_letter = letters.index(letter)
+      rotated_index = if direction == :right
+                        index_of_current_letter + offset
+                      else
+                        index_of_current_letter - offset
+                      end
+
+      if rotated_index > 7
+        rotated_index = rotated_index - 8
+      elsif rotated_index < 0
+        rotated_index = 8 + rotated_index
+      end
+
+      letters[rotated_index]
+    end
+
+    def up_up_left?(start_pos, end_pos)
+      (start_pos[0].to_i + 2) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :left, 1) == end_pos[1]
+    end
+
+    def up_up_right?(start_pos, end_pos)
+      (start_pos[0].to_i + 2) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :right, 1) == end_pos[1]
+    end
+
+    def up_left_left?(start_pos, end_pos)
+      (start_pos[0].to_i + 1) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :left, 2) == end_pos[1]
+    end
+
+    def up_right_right?(start_pos, end_pos)
+      (start_pos[0].to_i + 1) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :right, 2) == end_pos[1]
+    end
+
+    def down_left_left?(start_pos, end_pos)
+      (start_pos[0].to_i - 1) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :left, 2) == end_pos[1]
+    end
+
+    def down_right_right?(start_pos, end_pos)
+      (start_pos[0].to_i - 1) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :right, 2) == end_pos[1]
+    end
+
+    def down_down_left?(start_pos, end_pos)
+      (start_pos[0].to_i - 2) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :left, 1) == end_pos[1]
+    end
+
+    def down_down_right?(start_pos, end_pos)
+      (start_pos[0].to_i - 2) == end_pos[0].to_i &&
+        rotate_letter(start_pos[1], :right, 1) == end_pos[1]
+    end
   end
 end
--- spec/rchess/knight_spec.rb
+++ spec/rchess/knight_spec.rb
@@ -8,4 +8,76 @@ describe Rchess::Knight do
     expect(lowercase.letter).to eq "n"
     expect(uppercase.letter).to eq "N"
   end
+
+  describe "movement on the board" do
+    let(:knight) { Rchess::Knight.new(:uppercase) }
+    let(:start_position) { "5d" }
+
+    it "recognizes illegal moves" do
+      illegal_move = "5c"
+      expect(
+        knight.can_move_to_position? start_position, illegal_move
+      ).to eq false
+    end
+
+    describe "moving upward" do
+      it "can move one up and two left" do
+        end_position = "6b"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+
+      it "can move one up and two right" do
+        end_position = "6f"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+
+      it "can move two up and one left" do
+        end_position = "7c"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+
+      it "can move two up and one right" do
+        end_position = "7e"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+    end
+
+    describe "moving downward" do
+      it "can move one down and two left" do
+        end_position = "4b"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+
+      it "can move one down and two right" do
+        end_position = "4f"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+
+      it "can move two down and one left" do
+        end_position = "3c"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+
+      it "can move two down and one right" do
+        end_position = "3e"
+        expect(
+          knight.can_move_to_position? start_position, end_position
+        ).to eq true
+      end
+    end
+  end
 end
