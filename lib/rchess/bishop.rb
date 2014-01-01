7b327a85ba2e806b036f3f97c20694fca3fcfe1d
Eno Compton 4 <eno4@ecom.com>
2014-01-01 15:09:01 -0800

    Add Bishop#can_move_to_position?

--- lib/rchess/bishop.rb
+++ lib/rchess/bishop.rb
@@ -15,5 +15,12 @@ module Rchess
     def letter
       @lettercase == :uppercase ? "B" : "b"
     end
+
+    def can_move_to_position?(start_pos, end_pos)
+      file_difference = letter_difference(start_pos[0], end_pos[0])
+      rank_difference = rank_difference(start_pos[1], end_pos[1])
+
+      file_difference.abs == rank_difference.abs
+    end
   end
 end
--- spec/rchess/bishop_spec.rb
+++ spec/rchess/bishop_spec.rb
@@ -8,4 +8,56 @@ describe Rchess::Bishop do
     expect(lowercase.letter).to eq "b"
     expect(uppercase.letter).to eq "B"
   end
+
+  describe "movement on the board" do
+    let(:bishop) { Rchess::Bishop.new(:uppercase) }
+    let(:starting_point) { "d5" }
+
+    it "can move to the upper-right" do
+      end_position = "f7"
+      expect(
+        bishop.can_move_to_position?(starting_point, end_position)
+      ).to eq true
+    end
+
+    it "can move to the upper-left" do
+      end_position = "b7"
+      expect(
+        bishop.can_move_to_position?(starting_point, end_position)
+      ).to eq true
+    end
+
+    it "can move to the lower-right" do
+      end_position = "g2"
+      expect(
+        bishop.can_move_to_position?(starting_point, end_position)
+      ).to eq true
+    end
+
+    it "can move to the lower-left" do
+      end_position = "b3"
+      expect(
+        bishop.can_move_to_position?(starting_point, end_position)
+      ).to eq true
+    end
+
+    it "can move one square or multiple squares" do
+      one_square = "e6"
+      expect(
+        bishop.can_move_to_position?(starting_point, one_square)
+      ).to eq true
+
+      two_squares = "g8"
+      expect(
+        bishop.can_move_to_position?(starting_point, two_squares)
+      ).to eq true
+    end
+
+    it "recognizes illegal moves" do
+      illegal_move = "a5"
+      expect(
+        bishop.can_move_to_position?(starting_point, illegal_move)
+      ).to eq false
+    end
+  end
 end
