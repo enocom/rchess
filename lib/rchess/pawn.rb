c0cfafb31aca070230582da8028b1e0d04b8de86
Eno Compton 4 <eno4@ecom.com>
2014-01-01 20:45:27 -0800

    Refactor pawn movement logic

--- lib/rchess/pawn.rb
+++ lib/rchess/pawn.rb
@@ -18,44 +18,32 @@ module Rchess
     end
 
     def can_move_to_position?(start_pos, end_pos, option=:no_capture)
-      return capture_move?(start_pos, end_pos) if option == :capture
+      return valid_capture_move?(start_pos, end_pos) if option == :capture
 
       # uppercase pawns move down
       if lettercase == :uppercase
-        return one_square_downward?(start_pos, end_pos)
+        return one_square?(:down, start_pos, end_pos)
       end
 
       # lowercase pawns move up
-      one_square_forward?(start_pos, end_pos)
+      one_square?(:up, start_pos, end_pos)
     end
 
     private
-    def one_square_forward?(start_pos, end_pos)
-      rank_difference(start_pos[1], end_pos[1]) == 1 &&
-        letter_difference(start_pos[0], end_pos[0]) == 0
+    def one_square?(direction_y, start_pos, end_pos, direction_x=:straight)
+      dy = direction_y == :up ? 1 : -1
+      dx = direction_x == :straight ? 0 : 1
+
+      rank_difference(start_pos[1], end_pos[1]) == dy &&
+        letter_difference(start_pos[0], end_pos[0]).abs == dx
     end
 
-    def one_square_downward?(start_pos, end_pos)
-      rank_difference(start_pos[1], end_pos[1]) == -1 &&
-        letter_difference(start_pos[0], end_pos[0]) == 0
-    end
-
-    def capture_move?(start_pos, end_pos)
+    def valid_capture_move?(start_pos, end_pos)
       if lettercase == :uppercase
-        return one_diagonal_downward?(start_pos, end_pos)
+        return one_square?(:down, start_pos, end_pos, :diagonal)
       end
 
-      one_diagonal_forward?(start_pos, end_pos)
-    end
-
-    def one_diagonal_downward?(start_pos, end_pos)
-      rank_difference(start_pos[1], end_pos[1]) == -1 &&
-        letter_difference(start_pos[0], end_pos[0]).abs == 1
-    end
-
-    def one_diagonal_forward?(start_pos, end_pos)
-      rank_difference(start_pos[1], end_pos[1]) == 1 &&
-        letter_difference(start_pos[0], end_pos[0]).abs == 1
+      one_square?(:up, start_pos, end_pos, :diagonal)
     end
   end
 end
