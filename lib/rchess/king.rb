17730ed781a3adb11cb0732b6de18aa5b90df38f
Eno Compton 4 <eno4@ecom.com>
2014-01-01 16:46:00 -0800

    Add rough King#can_move_to_position?

--- lib/rchess/king.rb
+++ lib/rchess/king.rb
@@ -1,5 +1,7 @@
 module Rchess
   class King
+    include Rchess::BoardMath
+
     attr_reader :name, :lettercase
 
     def initialize(lettercase=:uppercase)
@@ -13,6 +15,23 @@ module Rchess
 
     def letter
       @lettercase == :uppercase ? "K" : "k"
+    end
+
+    def can_move_to_position?(start_pos, end_pos)
+      one_square_move?(start_pos, end_pos)
+    end
+
+    private
+    def one_square_move?(start_pos, end_pos)
+      start_file, end_file = start_pos[0], end_pos[0]
+      start_rank, end_rank = start_pos[1], end_pos[1]
+
+      (letter_difference(start_file, end_file).abs == 1 &&
+       rank_difference(start_rank, end_rank).abs == 0) ||
+      (rank_difference(start_rank, end_rank).abs == 1 &&
+       letter_difference(start_file, end_file).abs == 0) ||
+      (letter_difference(start_file, end_file).abs == 1 &&
+       rank_difference(start_rank, end_rank).abs == 1)
     end
   end
 end
--- spec/rchess/king_spec.rb
+++ spec/rchess/king_spec.rb
@@ -8,4 +8,33 @@ describe Rchess::King do
     expect(lowercase.letter).to eq "k"
     expect(uppercase.letter).to eq "K"
   end
+
+  describe "moving on the board" do
+    let(:king) { Rchess::King.new(:uppercase) }
+    let(:starting_point) { "d5" }
+
+    it "moves one square vertically or horizontally" do
+      %w(d6 d4 c5 e5).each do |new_position|
+        expect(
+          king.can_move_to_position?(starting_point, new_position)
+        ).to eq true
+      end
+    end
+
+    it "moves one square diagonally" do
+      %w(c4 c6 e6 e4).each do |new_position|
+        expect(
+          king.can_move_to_position?(starting_point, new_position)
+        ).to eq true
+      end
+    end
+
+    it "recognizes illegal moves" do
+      %w(b5 b4).each do |illegal_move|
+        expect(
+          king.can_move_to_position?(starting_point, illegal_move)
+        ).to eq false
+      end
+    end
+  end
 end
