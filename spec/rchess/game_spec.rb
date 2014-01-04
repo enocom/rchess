0a787983590ca15743f716f1a2437806b5223318
Eno Compton 4 <eno4@ecom.com>
2014-01-03 19:09:48 -0800

    Organize game spec

--- spec/rchess/game_spec.rb
+++ spec/rchess/game_spec.rb
@@ -7,13 +7,15 @@ describe Rchess::Game do
   let(:printer) { Rchess::Printer.new(fake_stdout) }
   let(:board) { Rchess::Board.new }
 
-  it "clears the screen, prints a starting board, and prompts the user" do
-    game = Rchess::Game.new(printer, board, fake_stdin)
+  describe "initialization" do
+    it "clears the screen, prints a starting board, and prompts the user" do
+      game = Rchess::Game.new(printer, board, fake_stdin)
 
-    expect(fake_stdout).to receive(:print).with(clear_screen)
-    expect(fake_stdout).to receive(:print).with(starting_board)
-    expect(fake_stdout).to receive(:print).with(user_prompt)
-    game.play_one_turn
+      expect(fake_stdout).to receive(:print).with(clear_screen)
+      expect(fake_stdout).to receive(:print).with(starting_board)
+      expect(fake_stdout).to receive(:print).with(user_prompt)
+      game.play_one_turn
+    end
   end
 
   it "accepts user input and starts another turn" do
@@ -26,25 +28,27 @@ describe Rchess::Game do
     game.next_turn
   end
 
-  it "prints a warning after bad user input" do
-    fake_stdin.stub(:gets).and_return("bananas\n")
-    game = Rchess::Game.new(printer, board, fake_stdin)
+  describe "bad or illegal input" do
+    it "prints a warning after bad user input" do
+      fake_stdin.stub(:gets).and_return("bananas\n")
+      game = Rchess::Game.new(printer, board, fake_stdin)
 
-    expect(fake_stdout).to receive(:print).with(clear_screen)
-    expect(fake_stdout).to receive(:print).with(starting_board)
-    expect(fake_stdout).to receive(:print).with(bad_input_message)
-    expect(fake_stdout).to receive(:print).with(user_prompt)
-    game.next_turn
-  end
+      expect(fake_stdout).to receive(:print).with(clear_screen)
+      expect(fake_stdout).to receive(:print).with(starting_board)
+      expect(fake_stdout).to receive(:print).with(bad_input_message)
+      expect(fake_stdout).to receive(:print).with(user_prompt)
+      game.next_turn
+    end
 
-  it "prints an error if the move is illegal" do
-    fake_stdin.stub(:gets).and_return("nd5\n")
-    game = Rchess::Game.new(printer, board, fake_stdin)
+    it "prints an error if the move is illegal" do
+      fake_stdin.stub(:gets).and_return("nd5\n")
+      game = Rchess::Game.new(printer, board, fake_stdin)
 
-    expect(fake_stdout).to receive(:print).with(clear_screen)
-    expect(fake_stdout).to receive(:print).with(starting_board)
-    expect(fake_stdout).to receive(:print).with(illegal_move_message)
-    expect(fake_stdout).to receive(:print).with(user_prompt)
-    game.next_turn
+      expect(fake_stdout).to receive(:print).with(clear_screen)
+      expect(fake_stdout).to receive(:print).with(starting_board)
+      expect(fake_stdout).to receive(:print).with(illegal_move_message)
+      expect(fake_stdout).to receive(:print).with(user_prompt)
+      game.next_turn
+    end
   end
 end
